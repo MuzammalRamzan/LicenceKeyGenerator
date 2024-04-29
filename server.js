b2b5692fdc49a3963ba0368f40645970b216940a
ArbitechSolution <arbitechsolutions@gmail.com>
2024-04-29 03:05:11 -0700

    updated server.js file

--- server.js
+++ server.js
@@ -10,28 +10,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI; // Ensure this variable is defined in your .env file
 
-const whitelist = ["https://www.theflipkitchen.com", "http://127.0.0.1:8000", "http://localhost:8000"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-    "device-remember-token",
-    "Access-Control-Allow-Origin",
-    "Origin",
-    "Accept",
-  ],
-};
+// const whitelist = ["https://www.theflipkitchen.com", "http://127.0.0.1:8000", "http://localhost:8000"];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+//   allowedHeaders: [
+//     "Content-Type",
+//     "Authorization",
+//     "X-Requested-With",
+//     "device-remember-token",
+//     "Access-Control-Allow-Origin",
+//     "Origin",
+//     "Accept",
+//   ],
+// };
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -40,7 +40,7 @@ mongoose
 
     // Start the Express server only after the database connection is established
     app.use(express.json());
-    app.use(cors(corsOptions));
+    app.use(cors);
     app.use("/api", routes);
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
